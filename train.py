e5f8a69bd7545d251ba10d2a47cc28c3072dd120
Artyom Bebroy <artyom.vasilenko.99@list.ru>
2026-06-29 15:11:57 +0500

    Update train.py

--- train.py
+++ train.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
     parser.add_argument('--name', type=str, required=True)
     parser.add_argument('--batch_size', type=int, required=True)
 
-    args = argparse.parse_args()
+    args = parser.parse_args()
 
     try:
         train(args.name, args.batch_size)
